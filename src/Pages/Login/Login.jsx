9aeaf75a8d0b36ecf3fb97d099830655f3de9453
Marcus uwu <viniciusdahora2013@gmail.com>
2022-04-21 19:04:22 -0300

    branch login

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="longin.scss" />
+export default function Login(){
+    return(
+        <div className="body">
+            <img className="Logo" src="" alt="" />
+            <img className="Logo-name" src="" alt="" />
+            <p className="title" ></p>
+        </div>
+    )
+}
