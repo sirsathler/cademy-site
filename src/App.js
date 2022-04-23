fe8c1c9b158b8df54c93221ad8105d4680c87ef9
unknown <rivaldocardoso2010@hotmail.com>
2022-04-23 12:34:14 -0300

    adicionando variaveis globais para o projeto

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import logo from './assets/images/logo-big.png';
 import './App.scss';
 import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom'
 
-import PageClass from './Pages/PageClass/Class'
+import Class from './Pages/Class/Class'
 
 function App() {
   return (
     <Router>
       <div className="App">
           <Routes>
-            <Route path='/class' element={<PageClass/>}/>
+            <Route path='/class' element={<Class/>}/>
           </Routes>
       </div>
     </Router>
