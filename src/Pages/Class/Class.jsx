314b9dd92c2fe1b9e54d76ec436bc5957f275587
unknown <rivaldocardoso2010@hotmail.com>
2022-04-24 10:47:47 -0300

    iniciando page Class

--- src/Pages/Class/Class.jsx
+++ src/Pages/Class/Class.jsx
@@ -2,8 +2,18 @@ import css from './Class.scss'
 
 export default function Class(){
     return(
-        <div>
-            
+        <div className='page-class'>
+            <section className='subject'>
+                
+            </section>
+            <main className='activities'>
+                <section className='to-done'>
+                    <p className='to-done__title'>Atividades</p>
+                </section>
+                <section className='to-done'>
+                    <p className='to-done__title'>Finalizados</p>
+                </section>
+            </main>
         </div>
     )
 }
