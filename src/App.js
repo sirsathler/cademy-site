8cf833a39bf589438599eacb41106b12c0e86679
unknown <rivaldocardoso2010@hotmail.com>
2022-04-27 15:36:42 -0300

    arquivo preparado para receber os cards e o carousel

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './assets/images/logo-big.png';
 import './App.scss';
 import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom'
 
-import Class from './components/Class/Class'
+import Class from './components/Pages/Class/Class'
 
 function App() {
   return (
